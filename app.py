1c364ee16e599f4bc60d30ee1d13ce7e83218efe
kome0024 <taku11240727@gmail.com>
2026-02-19 12:04:47 +0900

    app.py最後の行をディプロイ用に変更

--- app.py
+++ app.py
@@ -86,5 +86,8 @@ def internal_error(error):
     log.logger.error(f"500 error: {error}")
     return render_template("500.html"), 500 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
